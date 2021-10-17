26f4ada04b499bd04d3b0c5295d389283a20693d
James Karlinski <89712334+jamesk08@users.noreply.github.com>
2021-10-17 12:05:49 -0400

    Initial commit
    adding todo list gradle javafx app project stubs for assignment 4
    
    adding test stub
    
    Delete .gradle directory

---
+++ src/test/java/ucf/assignments/TodoListAppTests.java
@@ -0,0 +1,14 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TodoListAppTests
+{
+    @Test
+    public void Foo_Should_Return_Baz()
+    {
+        assertTrue(true);
+    }
+}
